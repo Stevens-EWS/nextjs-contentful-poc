4be8bf9cfef7396ce4479cb9e5f6b6584ed39620
Michael Forbes <mforbes@stevens.edu>
2022-01-21 01:41:24 -0500

    Brought all cms-contentful-app changes up one level (creating cms-contentful-app was a mistake)

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -1,8 +1,16 @@
-export default async function exit(_, res) {
+export default async function exit(req, res) {
+  const { returnto } = req.query
+  const unsafeReturnTo = decodeURIComponent(returnto)
+
+  // Prevent open redirect by ensuring initial slash (but not double slash) in return path.
+  const safeReturnTo = (unsafeReturnTo.charAt(0) === '/' && unsafeReturnTo.charAt(1) !== '/')
+    ? unsafeReturnTo
+    : '/'
+
   // Exit the current user from "Preview Mode". This function accepts no args.
   res.clearPreviewData()
 
   // Redirect the user back to the index page.
-  res.writeHead(307, { Location: '/' })
+  res.writeHead(307, { Location: safeReturnTo })
   res.end()
 }
