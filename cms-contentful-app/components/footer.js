20f00f204fcf9f45e09d59734c32e225d0c93839
Michael Forbes <mforbes@stevens.edu>
2022-01-20 23:23:21 -0500

    Replaced footer with Stevens copyright

--- cms-contentful-app/components/footer.js
+++ cms-contentful-app/components/footer.js
@@ -6,9 +6,7 @@ export default function Footer() {
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-1xl lg:text-2xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Platform Integration & Automation
-          </h3>
+          &copy; 2022 Stevens Institute of Technology
         </div>
       </Container>
     </footer>
